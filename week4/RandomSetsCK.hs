f321b4870bfc505aa6799c62a061bf6c39b2ac9c
don <don@ubuntu.(none)>
2013-09-24 22:57:01 +0200

    hlint

--- week4/RandomSetsCK.hs
+++ week4/RandomSetsCK.hs
@@ -7,10 +7,6 @@ import Techniques
 -- Task 2 : Implement a random data generator for the datatype Set Int
 --
 
---
--- @todo : from Set a to Set Int ?
---
-
 randomS:: IO (Set Int)
 randomS = do randomCount <- getRandomInt 10
              randomSet randomCount
