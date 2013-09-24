23e4b15a9bd1fcc9f16bbe5f52fb3d81e4421c69
mrluka <luc0@hotmail.de>
2013-09-24 20:02:34 +0200

    cleaned and renamed elements

--- week3/FormulaGenFOL.hs
+++ week3/FormulaGenFOL.hs
@@ -3,20 +3,19 @@ import Week3
 import Techniques
 
 
-nameList = [x,y,z,zero]
+termList = [x,y,z,zero]
 --
 getRandomFo :: Int -> IO [Formula]
-getRandomFo n = do d <- getRandomInt (length nameList)
-                   getRandomFormulas d n [] -- [(nameList !! 0)] -- deepth, n count
+getRandomFo n = do d <- getRandomInt 3 --(length termList)
+                   getRandomFormulas d n [] -- [(termList !! 0)] -- deepth, n count
 
 getRandomFormulas :: Int -> Int -> [Term] -> IO [Formula]
 getRandomFormulas _ 0 _ = return []
-getRandomFormulas d n names = do
-                  f <- getRandomFormula d names
-                  fs <- getRandomFormulas d (n-1) names
+getRandomFormulas d n terms = do
+                  f <- getRandomFormula d terms
+                  fs <- getRandomFormulas d (n-1) terms
                   return (f:fs)
 
-p = Atom "P"
 
 -- > Atomic: 
 --           NO bound variables, only free. IFF only in this actomic formula!
@@ -29,55 +28,56 @@ p = Atom "P"
 
 getRandomFormula :: Int ->[Term] -> IO Formula
 getRandomFormula 0 xs = if (length xs) == 0
-                           then do let atName = (getTermName xs)
+                           then do let aTerm = (getTermName xs)
                                    randomTermInt <- (getRandomInt 1)
                                    if (randomTermInt == 0)
                                       then return (r [z])
-                                      else return (r [z, atName])
+                                      else return (r [z, aTerm])
                            else  return (r (xs))
 
-getRandomFormula d names = do n<- getRandomInt 7
+getRandomFormula d terms = do n<- getRandomInt 7
                               case n of
---0 -> do f <- getRandomFormula 0 names  -- Atom Name [Term]
---return  f
-
                                    0 -> do -- Eq Term Term
                                            return (Eq (V "x") (V "y")) 
                                    
-                                   1 -> do fs <- getRandomFormula (d-1) names -- Neg Formula
+                                   1 -> do fs <- getRandomFormula (d-1) terms -- Neg Formula
                                            return (Neg (fs)) 
 
-                                   2 -> do f1 <- getRandomFormula (d-1) names -- Impl Formula Formula
-                                           let unboundV = (getTermName names)
-                                           f2 <- getRandomFormula (d-1) (reverse (unboundV:names))
+                                   2 -> do f1 <- getRandomFormula (d-1) terms -- Impl Formula Formula
+                                           let unboundV = (getTermName terms)
+                                           f2 <- getRandomFormula (d-1) (reverse (unboundV:terms))
                                            return (Impl f1 f2)
 
-                                   3 -> do f1 <- getRandomFormula (d-1) names -- Equi Formula Formula
-                                           let unboundV = (getTermName names)
-                                           f2 <- getRandomFormula (d-1) (reverse (unboundV:names))
+                                   3 -> do f1 <- getRandomFormula (d-1) terms -- Equi Formula Formula
+                                           let unboundV = (getTermName terms)
+                                           f2 <- getRandomFormula (d-1) (reverse (unboundV:terms))
                                            return (Equi f1 f2)
                                             
-                                   4 -> do f1 <- getRandomFormula (d-1) names -- Conj [Formula] 
-                                           let unboundV = (getTermName names)
-                                           f2 <- getRandomFormula (d-1) (reverse (unboundV:names))
+                                   4 -> do f1 <- getRandomFormula (d-1) terms -- Conj [Formula] 
+                                           let unboundV = (getTermName terms)
+                                           f2 <- getRandomFormula (d-1) (reverse (unboundV:terms))
                                            return (Conj [f1,f2])
 
-                                   5 -> do f1 <- getRandomFormula (d-1) names -- Conj [Formula]
-                                           f2 <- getRandomFormula (d-1) (reverse names) 
+                                   5 -> do f1 <- getRandomFormula (d-1) terms -- Conj [Formula]
+                                           f2 <- getRandomFormula (d-1) (reverse terms) 
                                            return (Disj [(f1),(f2)])
 
                                    6 -> do m <- getRandomInt 8 -- Forall Name Formula
-                                           let ad = getTermName names
-                                           fs <- getRandomFormula (d-1) (ad:names) 
+                                           let ad = getTermName terms
+                                           fs <- getRandomFormula (d-1) (ad:terms) 
                                            return (Forall  ((varsInTerm ad) !!0) (fs)) 
 
                                    7 -> do m <- getRandomInt 8 -- Exists Name formula
-                                           let ad  = (getTermName names)
-                                           fs <- getRandomFormula (d-1) (ad:names) 
+                                           let ad  = (getTermName terms)
+                                           fs <- getRandomFormula (d-1) (ad:terms) 
                                            return (Exists ((varsInTerm ad) !!0) (fs)) 
                            
 
 getTermName:: [Term] -> Term
-getTermName names | (length names) >= (length nameList) = zero
-                  | otherwise =  (nameList !! ((length names))) 
+getTermName terms | (length terms) >= (length termList) = x
+                  | otherwise =  (termList !! ((length terms))) 
 
+
+
+--0 -> do f <- getRandomFormula 0 names  -- Atom Name [Term]
+--return  f
