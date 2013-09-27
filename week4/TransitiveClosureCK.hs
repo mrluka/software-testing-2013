28fd3e6b134e16d3692188176099f04751d4ab3f
mrluka <luc0@hotmail.de>
2013-09-27 23:03:55 +0200

    Transitive closure tests update

--- week4/TransitiveClosureCK.hs
+++ week4/TransitiveClosureCK.hs
@@ -3,6 +3,7 @@ module TransitiveClosureCK where
 import SetOrd
 import Data.List
 import SetOperationsCK
+import Techniques
 --
 -- Task 4: (time spent: 6h)
 -- Suppose we implement binary relations as list of pairs, Haskell type [(a,a)].
@@ -18,10 +19,10 @@ r @@ s = nub [ (x,z) | (x,y) <- r, (w,z) <- s, y == w ]
 
 
 --
--- Use this to implement a function rClos :: Ord a => Rel a -> Rel a 
+-- Use this to implement a function trClos :: Ord a => Rel a -> Rel a 
 -- that gives the transitive closure of a relation, where the relation is
 -- represented as a list of pairs.
--- E.g., trClos [(1,2),(2,3),(3,4)] should give
+-- E.g., ttrClos [(1,2),(2,3),(3,4)] should give
 -- [(1,2),(1,3),(1,4),(2,3),(2,4),(3,4)].
 -- 
 
@@ -30,23 +31,23 @@ s2 = [(3,1),(3,5),(5,9),(1,9)]
 s3 = [(1,2),(2,3)]
 
 -- @todo cleanup implementation
-rClos :: Ord a => Rel a -> Rel a
-rClos x = setToRel ( rClos1 ( relToSet x ) )
+trClos :: Ord a => Rel a -> Rel a
+trClos x = setToRel ( trClos1 ( relToSet x ) )
 
-rClos1 :: Ord a => Set (a,a) -> Set (a,a)
-rClos1 x = x `SetOperationsCK.union` rClos2 x emptySet
+trClos1 :: Ord a => Set (a,a) -> Set (a,a)
+trClos1 x = x `SetOperationsCK.union` trClos2 x emptySet
 
-rClos2 :: Ord a => Set(a,a) -> Set(a,a) -> Set(a,a)
-rClos2 (Set []) y = y
-rClos2 (Set (x:xs)) y = rClos2 (Set xs) (rClos3 (Set (x:xs)) x y )
+trClos2 :: Ord a => Set(a,a) -> Set(a,a) -> Set(a,a)
+trClos2 (Set []) y = y
+trClos2 (Set (x:xs)) y = trClos2 (Set xs) (trClos3 (Set (x:xs)) x y )
 
-rClos3 (Set []) current z = z
-rClos3 (Set (x:xs)) current z = rClos4 current (getAllSuccessors (Set (x:xs)) (getFirst x)) z
+trClos3 (Set []) current z = z
+trClos3 (Set (x:xs)) current z = trClos4 current (getAllSuccessors (Set (x:xs)) (getFirst x)) z
 
-rClos4 current (Set []) z = z
-rClos4 current (Set (y:ys)) z = rClos4 current (Set ys) (rClos5 current y z)
+trClos4 current (Set []) z = z
+trClos4 current (Set (y:ys)) z = trClos4 current (Set ys) (trClos5 current y z)
 
-rClos5 current y z =  if inSet (getFirst current,y) z
+trClos5 current y z =  if inSet (getFirst current,y) z
 				      then z 
 				      else insertSet (getFirst current,y) z
 
@@ -104,19 +105,76 @@ getSecond :: (a,b) -> b
 getSecond (a,b) = b
 
 
--- Task 5:
+-- > ---- Task 5:
+-------
+-------------------Bug: trClosM if called e.g. with: [(1,2),(2,1)] or.  [(3,4),(4,3),(1,3)] :::: . ...
+------------------- Seems to be the [(x,y),(y,x)] pattern. No further investigations  :: :: : . ....
+------------------simpleAutoTest exits or not, depends on randomly generated pairs, as mentioned ^ 
+-------
+
+
+
 -- Test the function trClos from the previous exercise. Devise your own 
 -- test method for this. Try to use random test generation. Define reasonable properties to test
+
+
+-- Possible way: (Not implemented yet) (P. 176 shows how this could work)
+--1a) Generate random Pairs/Relations [(x,y),..,(xn,yn)] and store the pairs separately.
+--b) Then ask the trClos function to make transitive closure of these relations.
+--c) Then check if the resulting relation is transitive.
+--d) Do another check by manually going through the relations from a) and check if needed counterparts are present. 
+
+--2) Use different algorithms, like in the following part.
+
+-----manual
+manualTest = (not (isTrans s1) &&  isTrans (trClos s1)) && not (isTransitiveClosure s1) &&  isTransitiveClosure (trClos s1)
+
+
+-----automatic
+simpleAutoTest :: IO Bool
+simpleAutoTest = autoTest 1
+
 --
 
+
+-- maybe more complex tests with more parameters :test count, pair count [(),....()n], int element range n (x E n)
+autoTest :: Int ->IO Bool
+autoTest testCount  = if testCount > 0 then do 
+                                               randRel <- (getRandomRelation 2)  -- relation pair count
+                                               let tr = (trClos randRel)
+                                               testRes <-  (autoTest (testCount -1))
+                                               testResR <- (autoTestR tr)
+                                               return (testRes && testResR)
+                      else return True
+
+
+
+-- 2 different proofs for transitivity 
+autoTestR :: Rel Int -> IO Bool
+autoTestR x = return ((isTransitiveClosure x || isTransitiveClosure (trClos x)) || ((isTrans x) || (isTrans (trClos x))))
+--autoTestR2 x = return (isTransitiveClosure x || isTransitiveClosure (trClos x)) -- method #1
+--autoTestR2 x = return ((isTrans x) || (isTrans (trClos x))) -- method #2
+
+
+
+getRandomRelation ::  Int -> IO (Rel Int) 
+getRandomRelation  0 = return []
+getRandomRelation  n = do fstX <- getRandomInt 3 -- x range
+			  fstY <- getRandomInt 3 -- y range				
+			  randPairs <- getRandomRelation (n-1) 			
+			  return ((fstX, fstY) : randPairs)
+
+
+
+-- -- -- -- Transitive check method #1
 -- here i can reuse the functions above:
 -- to prove that a relation is a transitive closure,
 -- the output of getSuccessors and getAllSuccessors 
 -- must be the same for each element in the relation
---
+
 isTransitiveClosure :: Ord a => Rel a -> Bool
 isTransitiveClosure x = isTransitiveClosureR (relToSet x)
-
+ 
 isTransitiveClosureR :: Ord a => Set (a,a) -> Bool
 isTransitiveClosureR (Set []) = True
 isTransitiveClosureR (Set (x:xs)) = isTransitiveIn x (Set (x:xs)) && isTransitiveClosureR (Set xs)
@@ -125,16 +183,10 @@ isTransitiveIn :: Ord a => (a,a) -> Set(a,a) -> Bool
 isTransitiveIn x (Set []) = True
 isTransitiveIn x (Set (y:ys)) = isEmpty(SetOperationsCK.difference (getAllSuccessors (Set (y:ys)) (getFirst x))  (getSuccessors (Set (y:ys)) (getFirst x)))
 
-manualTest = not (isTransitiveClosure s1) &&  isTransitiveClosure (rClos s1)
 
-autoTest :: Bool
-autoTest = autoTestR 1000
 
-autoTestR x = if x > 0 then autoTestR (x -1) && autoTestR2 getRandomRelation else True
-
--- @todo replace with real randomizer
-getRandomRelation :: Ord a => Num a=> Rel a
-getRandomRelation = setToRel (insertSet (1,2) ( insertSet (2,3) (insertSet (3,4) emptySet)))
-
-autoTestR2 :: Ord a => Rel a -> Bool
-autoTestR2 x = isTransitiveClosure x || isTransitiveClosure (rClos x)
+-- -- -- -- Transitive check method #2 (From book page 175. transR)
+isTrans :: Ord a => Rel a -> Bool
+isTrans ([]) = True
+isTrans ( s) = and [trans pair (Set s) | pair <- s] where       
+               trans (x,y) (Set r) = and [inSet (x,v) (Set r) | (u,v) <- r, u ==y]
